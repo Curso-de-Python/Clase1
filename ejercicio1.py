19d0f5f1055ab5f2a00045ce4794a634ea202bfe
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-09-29 13:34:06 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,6 @@
 #-----------------------------
 # EJERCICIO N°1
+# La función print()
 #-----------------------------
 
 # 1. Al final de las instrucciones observa cómo la función print() imprime la linea "¡Hola, Mundo!" en la pantalla.
