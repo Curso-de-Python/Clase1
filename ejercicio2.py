e7ee38cf7db8b18882566e228beeafdcf04a9caf
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-09-29 12:42:14 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,22 @@
 #-----------------------------
-# Ejercicio N°2
+# EJERCICIO N°2
 #-----------------------------
 
 # 1. Veamos primero dos instrucciones simples al final de las instrucciones
+
+# 2. Ahora, vamos a agregar una invocación vacía de la función print(), esta lo que genera es una línea vacía
+
+#--------------
+# LOS CARACTERES DE ESCAPE Y NUEVA LÍNEA
+
+# 3. Inserta un nuevo par de caracteres en tu oración: "\n", ¡en cualquier parte!
+
+# 4. Ahora una pregunta interesante, ¿cómo harías si quieres imprimir lo siguiente? *''Los caracteres \n se utilizan para añadir una nueva línea''*
+
+#--------------
+# ARGUMENTOS MÚLTIPLES
+
+# 5. Escribamos más de una cadena dentro de una misma función print(), separándolas con comas
+
+print("¡Hola! Mi nombre es Python.")
+print("Monty Python.")
