2aff7007cd155317ca4f9ca0048028a53aa1ebbd
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-09-29 12:16:22 -0500

    Update and rename holaMundo.py to ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+# Ejercicio N°1
+#-----------------------------
+
+# 1. Utiliza la función print() para imprimir la linea "¡Hola, Mundo!" en la pantalla.
+
+# 2. Ahora intenta imprimir tu nombre.
+
+# 3. Elimina las comillas dobles y ejecuta el código. ¿Qué tipo de error se produce?
+
+# 4. Elimina los paréntesis y vuelve a poner las comillas dobles. ¿Qué tipo de error se produce esta vez?
+
+# 5. ¡Experimenta tanto como puedas!
+
+print("¡Hola, mundo!")
