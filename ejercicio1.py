65c172e83b3e4689183fe54844c2b40b6b3a9f65
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-09-29 13:55:58 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,16 +1,13 @@
-#-----------------------------
-# EJERCICIO N°1
-# La función print()
-#-----------------------------
-
-# 1. Al final de las instrucciones observa cómo la función print() imprime la linea "¡Hola, Mundo!" en la pantalla.
-
-# 2. Ahora intenta imprimir tu nombre.
-
-# 3. Elimina las comillas dobles y ejecuta el código. ¿Qué tipo de error se produce?
-
-# 4. Elimina los paréntesis y vuelve a poner las comillas dobles. ¿Qué tipo de error se produce esta vez?
-
-# 5. ¡Experimenta tanto como puedas!
-
+'''
+-----------------------------
+ EJERCICIO N°1
+ La función print()
+-----------------------------
+1. Al final de las instrucciones observa cómo la función print() imprime la linea "¡Hola, Mundo!" en la pantalla.
+2. Ahora intenta imprimir tu nombre.
+3. Elimina las comillas dobles y ejecuta el código. ¿Qué tipo de error se produce?
+4. Elimina los paréntesis y vuelve a poner las comillas dobles. ¿Qué tipo de error se produce esta vez?
+5. ¡Experimenta tanto como puedas!
+-----------------------------
+'''
 print("¡Hola, mundo!")
