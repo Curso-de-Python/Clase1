13424ec5249f510ebaee6d82e7fdea05e2e77e66
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 08:41:27 -0500

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,24 @@
+''''
+-----------------------------
+ EJERCICIO N°3
+ Argumentos de palabras clave
+-----------------------------
+ "END"
+
+ 1. Observa cómo se utiliza el argumento end=" " al final de la función
+ 2. Prueba el valor vacío "", el carácter de nueva línea "\n" ¡y los símbolos que quieras!
+-----------------------------
+'''
+print("Mi nombre es", "Python.", end="")
+print("Monty Python.")
+
+'''
+-----------------------------
+ "SEP"
+
+ 1. Observa cómo se utiliza el argumento sep="-" al final de tus argumentos usuales
+ 2. Prueba el valor vacío "", el carácter de nueva línea "\n" ¡y los símbolos que quieras!
+ ¡Ahora puedes mezclar ambos! Utiliza cualquier símbolo que te venga a la mente
+-----------------------------
+'''
+#print("La","programación","es","lo","mejor", sep="-")
