333ed58bad6d4019a407f5abdb7c043f9a138a7c
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 07:30:35 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,23 +1,21 @@
-#-----------------------------
-# EJERCICIO N°2
-# Saltos de línea y argumentos múltiples
-#-----------------------------
+'''
+-----------------------------
+ EJERCICIO N°2
+ Saltos de línea y argumentos múltiples
+-----------------------------
+ 1. Veamos las instrucciones simples al final del código
+ 2. Agreguemos una invocación vacía: print()
+-----------------------------
+ LOS CARACTERES DE ESCAPE Y NUEVA LÍNEA
 
-# 1. Veamos primero dos instrucciones simples al final de las instrucciones
+ 3. Inserta "\n" en tu oración, ¡en cualquier parte!
+ 4. ¿Cómo imprimirías lo siguiente? 
+ "Los caracteres \n se utilizan para añadir una nueva línea"
+-----------------------------
+ ARGUMENTOS MÚLTIPLES
 
-# 2. Ahora, vamos a agregar una invocación vacía de la función print(), esta lo que genera es una línea vacía
-
-#--------------
-# LOS CARACTERES DE ESCAPE Y NUEVA LÍNEA
-
-# 3. Inserta un nuevo par de caracteres en tu oración: "\n", ¡en cualquier parte!
-
-# 4. Ahora una pregunta interesante, ¿cómo harías si quieres imprimir lo siguiente? *''Los caracteres \n se utilizan para añadir una nueva línea''*
-
-#--------------
-# ARGUMENTOS MÚLTIPLES
-
-# 5. Escribamos más de una cadena dentro de una misma función print(), separándolas con comas
-
+ 5. Escribamos más de una cadena dentro de un print(), separándolas con comas
+-----------------------------
+'''
 print("¡Hola! Mi nombre es Python.")
 print("Monty Python.")
