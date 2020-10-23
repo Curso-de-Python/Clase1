3e9ff16f922024384eeeb1a7973031db7a6b154a
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-22 21:56:32 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,12 @@
+# LA FUNCIÓN PRINT
+# ------------------
+# CARACTERES DE ESCAPE Y NUEVA LÍNEA
+
+print("¡Hola! \n¿Cómo te llamas?")
+print()
+print("\nMi nombre es","Python.","\nMonty Python.")
+
+# ARGUMENTOS DE PALABRAS CLAVE
+
+#print("Mi","nombre","es", sep="_", end="*\n")
+#print("Monty","Python.", sep="*", end="*")
